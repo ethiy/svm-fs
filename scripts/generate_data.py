be1d91a9d7dd58325d797948dda7708208dd7281
Ethiy <oussama.ennafii@ign.fr>
2017-12-01 16:26:35 +0100

    soft margin svm

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -12,25 +12,21 @@ def generate_guassian(mean, covar, samples, color, label, marker):
 
 def main():
     generate_guassian(
-        [200, 80],
-        [[25, 30], [15, 12]],
-        50,
+        [9, 0],
+        [[25, 30], [15, 0]],
+        100,
         'blue',
         '+1',
         ','
     )
     generate_guassian(
-        [170, 69],
-        [[20, 30], [15, 10]],
-        50,
+        [-10, 0],
+        [[20, 30], [15, 0]],
+        100,
         'red',
         '-1',
         '^'
     )
-
-    # plt.plot([180, 190], [40, 100], 'g--')
-    # plt.plot([190, 185], [40, 100], 'c--')
-    # plt.plot([185, 185], [40, 100], 'm--')
     plt.legend()
 
     plt.show()
