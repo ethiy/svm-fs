5c0f9f3b3e14a8b2ba4994049a65c2ebaf1d1719
Oussama ENNAFII <oussama.ennafii@polytechnique.org>
2017-12-04 02:35:41 +0100

    dual svm

---
+++ scripts/kernels.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def main():
+    X = np.linspace(-35, 35, 10000)
+    plt.plot(X, np.exp(-10 * pow(X, 2)), c='g', label='gamma=10')
+    plt.plot(X, np.exp(-.5 * pow(X, 2)), c='r', label='gamma=1/2')
+    plt.plot(X, np.exp(-.01 * pow(X, 2)), c='b', label='gamma=1/100')
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
